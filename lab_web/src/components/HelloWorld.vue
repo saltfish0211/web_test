4222b9a6f8aa79672b1bbe7b701183dac74524dc
saltfish0211 <yujimtw@gmail.com>
2023-04-22 22:04:31 +0800

    修改了helloworld並測試 key屬性

--- lab_web/src/components/HelloWorld.vue
+++ lab_web/src/components/HelloWorld.vue
@@ -1,45 +1,20 @@
 <script setup>
 import { ref } from "vue";
 
-// const data = "SF test2";
-// const text = ref("defult");
+const test = ref("test");
 
-// const view = ref(1);
-// const changeview = (index) => {
-//   view.value = index;
-// };
-const fordata = [
-  1,
-  2,
-  3,
-  4,
-  "sad",
-  "sdagf",
-  "??"
-]
+setTimeout(() => {
+  test.value = "test2";
+},2000)
 
 </script>
 
 <template>
-  <!-- <button v-on:click="changeview(1)">顯示單向綁定</button>
-  <button @click="changeview(2)">顯示雙向綁定</button>
-  <button @click="changeview(3)">沒有畫面</button>
-
-  <div class="container-1" v-if="view === 1">
-    <p>下面是單向綁定</p>
-    {{ data }}
-  </div>
-
-  <div class="container-2" v-if="view === 2">
-    <p>下面是雙向綁定</p>
-    <input type="text" v-model="text" />
-    <p>{{ text }}</p>
-  </div> -->
-
-  <!-- 這邊要注意if else的判斷大多是1對1，要不然就是你的else條件要寫完善一點 -->
-  <!-- <div v-if="view !== 1 && view !== 2">沒有畫面</div> -->
-  <p v-for="forlist in fordata">
-  {{ forlist }}
+  
+  <p class="special"
+  :key="test"
+  >
+  {{ test }} 
   </p>
 </template>
 
@@ -51,4 +26,19 @@ const fordata = [
 .container-2 {
   border: 2px skyblue solid;
 }
+
+.special {
+  font-size: 30px;
+  animation: openin 0.5s ease-in-out;
+}
+
+@keyframes openin {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
 </style>
